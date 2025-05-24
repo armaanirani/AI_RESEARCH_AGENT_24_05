c05389f0204ae2f11d22d9529fa1da23d61bcc07
Armaan Irani <armaanirani@gmail.com>
2025-05-24 19:04:10 +0530

    created a wikipedia tool

--- tools.py
+++ tools.py
@@ -10,3 +10,8 @@ search_tool = Tool(
     description="Serch the web for information.",
 )
 
+api_wrapper = WikipediaAPIWrapper(
+    top_k_results=1,
+    doc_content_chars_max=100,
+)
+wiki_tool = WikipediaQueryRun(api_wrapper=api_wrapper)
